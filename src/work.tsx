820eeb1880f02b7572317ff03699fce4a70d8e55
dalramjwi <dalramjwi@gmail.com>
2024-07-11 12:04:10 +0900

    divMaker type grandChildren 오류 발생 : unknown

--- src/work.tsx
+++ src/work.tsx
@@ -1,5 +1,23 @@
 import React from "react";
-const App = () => {
-  return <h1>asd</h1>;
+import { styles } from "./data";
+import { idAndChildren } from "./data";
+
+const divMaker = (id: string, children: string | number) => {
+  const div = document.createElement("div");
+  div.id = id;
+
+  if (typeof children === "object") {
+    for (const [childId, grandChildren] of Object.entries(children)) {
+      const childDiv = divMaker(childId, grandChildren);
+      div.appendChild(childDiv);
+    }
+  }
+
+  return div;
 };
+
+const App = () => {
+  return <App />;
+};
+
 export default App;
